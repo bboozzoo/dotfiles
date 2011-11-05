0215019f4d230997e078de90ad1a50427dc57a96
Maciek Borzecki <maciek.borzecki@gmail.com>
2011-11-05 15:54:53 +0100

    Emacs config cleanups.
    - Added bozo prefix to all functions in module-utils
    - Added bozo-load-module to interactively enable modules

--- config/emacs.d/modules/module-keys.el
+++ config/emacs.d/modules/module-keys.el
@@ -1,18 +1,20 @@
 
 ; key bindings
 (global-set-key (kbd "C-<tab>") 'other-frame)
-(global-set-key (kbd "C-<prior>") 'select-previous-window) ; ctr+pg_up
-(global-set-key (kbd "C-<next>") 'select-next-window) ; ctr+pg_down
-(global-set-key (kbd "C-<f12>") 'recentf-open-files)
+(global-set-key (kbd "C-<prior>") 'bozo-select-previous-window) ; ctr+pg_up
+(global-set-key (kbd "C-<next>") 'bozo-select-next-window) ; ctr+pg_down
+
 (global-set-key (kbd "s-o") 'jl-jump-backward)
 (global-set-key (kbd "s-i") 'jl-jump-forward)
-(global-set-key (kbd "C-5") 'goto-match-paren)
+(global-set-key (kbd "C-5") 'bozo-goto-match-paren)
 (global-set-key (kbd "C-Z") nil) ;disable ctrl + z background mode
-(global-set-key (kbd "C-<f5>") 'refresh-file)
+(global-set-key (kbd "C-<f5>") 'bozo-refresh-file)
 (global-set-key (kbd "C-<f8>") 'comment-or-uncomment-region) ; (un)comment
 (when (fboundp 'sr-speedbar-toggle)
   (global-set-key (kbd "C-<f9>") 'sr-speedbar-toggle)
   (global-set-key (kbd "C-<f10>") 'sr-speedbar-select-window)) ; speedbar
+(global-set-key (kbd "C-<f11>") 'bozo-load-module)
+(global-set-key (kbd "C-<f12>") 'recentf-open-files)
 
 (provide 'module-keys)
 ;; module-keys.el ends here
--- config/emacs.d/modules/module-util.el
+++ config/emacs.d/modules/module-util.el
@@ -1,18 +1,18 @@
 ;; set frame title manually
-(defun set-frame-title (title)
+(defun bozo-set-frame-title (title)
   "Set current frame title"
   (interactive "sNew frame title: ")
   (setq frame-title-format title)
   )
 
 ;; refresh file 
-(defun refresh-file ()
+(defun bozo-refresh-file ()
   (interactive)
   (revert-buffer t (not (buffer-modified-p)) t)
   )
 
 ;; jump to matching parenthesis
-(defun goto-match-paren (arg)
+(defun bozo-goto-match-paren (arg)
   "Go to the matching  if on (){}[], similar to vi style of % "
   (interactive "p")
   ;; first, check for "outside of bracket" positions expected by forward-sexp, etc.
@@ -23,10 +23,54 @@
         ((looking-back "[\[\(\{]" 1) (backward-char) (forward-sexp))
         (t nil)))
 
-(defun recompile-modules ()
+;; rebuild all modules in local-modules-dir
+(defun bozo-recompile-modules ()
   "Byte-compile all modules"
   (interactive)
   (byte-recompile-directory local-modules-dir 0)
+  )
+
+;; taken from http://nex-3.com/posts/45-efficient-window-switching-in-emacs
+(defun bozo-select-next-window ()
+  "Switch to the next window" 
+  (interactive)
+  (select-window (next-window)))
+
+(defun bozo-select-previous-window ()
+  "Switch to the previous window" 
+  (interactive)
+  (select-window (previous-window)))
+
+;; select function for completing read
+(defun bozo-pick-completing-read ()
+  "Pick a function for completing read"
+  (if (fboundp 'ido-completing-read)
+      'ido-completing-read
+    'completing-read)
+  )
+
+;; load modules interactively
+(defun bozo-load-module ()
+  "Load specific module"
+  (interactive)
+  (let (modules-list module-name module-file)
+    (setq modules-list
+          (mapcar (lambda (md) 
+                    (cadr (split-string md "[-\.]")))
+                  (directory-files local-modules-dir 
+                                   nil 
+                                   "module-[A-Za-z0-9]+\.el$")))
+    (setq module-name (funcall (bozo-pick-completing-read) 
+                               "Load module: " 
+                               modules-list))
+    (message "Loading module %s" module-name)
+    (setq module-file (concat "module-" module-name))
+    (message "module file %s" module-file)
+    ;; require wants a symbol, make one
+    (if (require (intern module-file) nil 0)
+        (message "Module %s loaded" module-name)
+        (message "Failed to load module %s (file %s)" module-name module-file))
+    )
   )
 
 (provide 'module-util)
