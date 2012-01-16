ef1792d9a8ac18a6fea97da2f23e7e2e918d6b5f
Maciek Borzecki <maciek.borzecki@gmail.com>
2012-01-16 21:46:53 +0100

    emacs: add autocomplete to dotfiles

--- config/emacs.d/modules/module-autocompletion.el
+++ config/emacs.d/modules/module-autocompletion.el
@@ -1,7 +1,14 @@
-(require 'auto-complete-config)
+;; default dir where auto-complete is installed
+(setq auto-complete-install-dir (expand-file-name 
+                                 "auto-complete" local-plugins-dir))
 
-(let ((default-dict-dir (package--dir "auto-complete" "1.4.20110207")))
-  (add-to-list 'ac-dictionary-directories (concat default-dict-dir "/dict")))
+(add-to-list 'load-path auto-complete-install-dir)
+
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories (expand-file-name
+                                         "ac-dict"
+                                         auto-complete-install-dir))
+(ac-config-default)
 
 (ac-config-default)
 (setq ac-delay 0)
@@ -9,6 +16,8 @@
 (global-auto-complete-mode t)
 
 (add-to-list 'ac-modes 'makefile-gmake-mode)
+(add-to-list 'ac-modes 'nxml-mode)
 
 
 (provide 'module-autocompletion)
+;; module-autocompletion.el ends here
