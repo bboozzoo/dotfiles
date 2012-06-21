1a15b07e7d60c0dd20525f6a7f3d6428ab3d5eb7
Maciek Borzecki <maciek.borzecki@gmail.com>
2012-06-21 09:35:01 +0200

    emacs: switch to melpa repository

--- config/emacs.d/modules/module-elpa.el
+++ config/emacs.d/modules/module-elpa.el
@@ -1,7 +1,9 @@
 (require 'package)
 
 ;; enable marmalade repository
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (package-initialize)
 
