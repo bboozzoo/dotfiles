6238673e895420a7af985f4da4923decac041e9c
Maciek Borzecki <maciek.borzecki@gmail.com>
2013-01-04 21:15:18 +0100

    emacs: keybindings for org-remember

--- config/emacs.d/modules/module-keys.el
+++ config/emacs.d/modules/module-keys.el
@@ -27,6 +27,8 @@
   (global-set-key (kbd "C-<f7>") 'fci-mode))
 ;; org mode
 (global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c j") 'org-remember)
+
 (global-set-key (kbd "C-c r") 'remember)
 ;;(global-set-key (kbd "C-c l") 'org-store-link)
 
