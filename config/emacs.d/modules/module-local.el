bd58b1427ed10816e481e65716b250cfbfd2c901
Maciek Borzecki <maciek.borzecki@gmail.com>
2013-01-04 21:14:52 +0100

    emacs: refine loading of local configs to skip backup files

--- config/emacs.d/modules/module-local.el
+++ config/emacs.d/modules/module-local.el
@@ -3,7 +3,7 @@
 
 (message "Load custom files from %s" local-site-custom-dir)
 
-(dolist (local-file (directory-files local-site-custom-dir t ".*.el$"))
+(dolist (local-file (directory-files local-site-custom-dir t "^[^#]*.el$"))
   (progn (message "Loading custom file %s" local-file)
          (load-file local-file)))
 
