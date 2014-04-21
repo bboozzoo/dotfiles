9f561340b219009e87623416ccf803bc32040906
Maciek Borzecki <maciek.borzecki@gmail.com>
2014-04-21 22:33:08 +0200

    emacs: enable marmalade repo

--- config/emacs.d/modules/module-elpa.el
+++ config/emacs.d/modules/module-elpa.el
@@ -1,7 +1,8 @@
 (require 'package)
 
 ;; enable marmalade repository
-;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
