2709d87e219328c9f471771ca4b9b07df467b621
Maciek Borzecki <maciek.borzecki@gmail.com>
2014-11-23 19:40:37 +0100

    emacs: switch to babel-style config

--- config/emacs.d/init.el
+++ config/emacs.d/init.el
@@ -1,58 +1,18 @@
-;; From Emacs Starter Kit
-;; Turn off mouse interface early in startup to avoid momentary display
-;; You really don't need these; trust me.
+(setq config-file "~/.emacs.d/maciek.org")
 
-(setq-default bozo-required-packages '())
-
-(setq required-packages-file "~/.emacs.d/required-packages.el")
-(setq local-modules-dir "~/.emacs.d/modules")
-(setq local-cache-dir "~/.emacs.d/cache/")
-(setq local-plugins-dir "~/.emacs.d/plugins")
-(setq local-site-custom-dir "~/.emacs.d/custom")
-(setq local-themes-custom-dir "~/.emacs.d/themes")
-
-(add-to-list 'load-path local-modules-dir)
-(add-to-list 'load-path "~/.emacs.d/plugins/")
 (add-to-list 'load-path "~/.emacs.d/elpa/")
-;; add plugins dir versioned after emacs major version
-(let ((extra-emacs-version-plugins-dir (concat local-plugins-dir
-                                       "/emacs-"
-                                       (number-to-string emacs-major-version))))
-  (if (file-directory-p extra-emacs-version-plugins-dir)
-      (add-to-list 'load-path extra-emacs-version-plugins-dir)))
 
-(require 'module-util)
+(require 'package)
 
-;; local cache path (for recentf, IDO, etc.)
-; Create cache dir if one does not exist
-(bozo-make-dir local-cache-dir)
+;; enable marmalade and MELPA repositories
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
-(setq bozo-color-theme-x 'naquadah)
-;(setq bozo-color-theme-terminal 'color-theme-tty-dark)
+(package-initialize)
 
-(require 'module-elpa)
-
-;; check if all required packages are installed
-; start with refreshing list of all known packages
-(bozo-refresh-package-list)
-
-; now do the update
-(if (file-exists-p required-packages-file)
-    (progn
-      (load-file required-packages-file)
-      (let ((missing-packages (bozo-list-missing-packages
-                               bozo-required-packages)))
-        (when missing-packages
-          (message "missing %d packages: %s"
-                   (length missing-packages) missing-packages)
-          (bozo-install-packages missing-packages)))))
-
-(require 'module-local)
-
-(require 'module-lang)
-(require 'module-ui)
-(require 'module-usability)
-(require 'module-devel)
-(require 'module-net)
-(require 'module-mail)
-(require 'module-keys)
+(require 'org)
+(require 'ob-tangle)
+(when (file-exists-p config-file)
+    (org-babel-load-file config-file))
