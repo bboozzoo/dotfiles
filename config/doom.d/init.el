60bd299b735e8c632f76c70c73ef197c210fcbeb
Maciek Borzecki <maciek.borzecki@gmail.com>
2024-09-18 09:53:03 +0200

    config/doom: disable evil, enable flyspell
    Signed-off-by: Maciek Borzecki <maciek.borzecki@gmail.com>

--- config/doom.d/init.el
+++ config/doom.d/init.el
@@ -53,7 +53,7 @@
        ;;zen               ; distraction-free coding or writing
 
        :editor
-       (evil +everywhere); come to the dark side, we have cookies
+       ;(evil +everywhere) ; come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
        ;;(format +onsave)  ; automated prettiness
@@ -82,7 +82,7 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       ;;(spell +flyspell) ; tasing you for misspelling mispelling
+       (spell +flyspell +everywhere) ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
